b3f0fb626a9a6ce844e9fe4ffd6532467e2dd344
NgMinhTie <ngminhtien1230@gmail.com>
2024-12-10 18:15:01 +0700

    Update 3 links

--- backend/index.js
+++ backend/index.js
@@ -127,13 +127,69 @@ app.post("/notknown3", async (req, res) => {
         PhoneNumber,
       ]);
 
+    //const query_result = await pool.promise().query("SELECT * from customer");
     return res.send(`Insert successful Customer ${accountID}`);
+    return res.send(query_result); //* TEST SUCCESSFUL
   } catch (err) {
     return res.send("Server cannot send response");
   }
 });
 //* PROCEDURE FOR INSERTING
 
+
+//! CODE BELOW IS IMPLEMENTING BACKEND BUT NOT HAVING DATA
+app.post("/notknown4", async (req, res) => {
+  try {
+    const { AccountID, NewPoints } = req.body;
+    const [result] = await pool.promise().query("CALL UpdateCustomerPoints(?,?)", [
+      AccountID, NewPoints,
+    ]);
+    return res.send("Updating successfully");
+  }
+  catch (err) {
+    return res.send("Server cannot send response");
+  }
+
+});
+
+app.post("/notknown5", async (req, res) => {
+  try {
+    const { PhoneNumber} = req.body;
+    const [result] = await pool
+      .promise()
+      .query("CALL DeleteCustomer(?)", [PhoneNumber]);
+    return res.send("Delete successfully");
+  } catch (err) {
+    return res.send("Server cannot send response");
+  }
+});
+
+//* FUNCTION
+app.post("/notknown6", async (req, res) => {
+  try {
+    const { AccountID } = req.body;
+    const [result] = await pool
+      .promise()
+      .query("CALL CalculateTotalDiscount(?) AS TotalDiscount", [AccountID]);
+    const totaldiscount = result.TotalDiscount;
+    return res.send(`Calculating successfully ${totaldiscount}`);
+  } catch (err) {
+    return res.send("Server cannot send response");
+  }
+});
+
+app.post("/notknown7", async (req, res) => {
+  try {
+    const { PhoneNumber } = req.body;
+    const [result] = await pool
+      .promise()
+      .query("CALL ValidatePhoneNumber(?) AS Valid", [PhoneNumber]);
+    const valid = result.Valid;
+    return res.send(`Check Valid successfully ${valid}`);
+  } catch (err) {
+    return res.send("Server cannot send response");
+  }
+});
 //* TEST CONNECTION
 // const query = 'SELECT * FROM customer';
 
